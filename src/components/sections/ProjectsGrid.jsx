2e3c452a2d290a1b7e0813e0bc0881d2d37d4cb3
임유진 <lyj5721@sookmyung.ac.kr>
2025-12-23 02:27:58 +0900

    feat: 카드 그리드 구현

--- src/components/sections/ProjectsGrid.jsx
+++ src/components/sections/ProjectsGrid.jsx
@@ -1,3 +1,34 @@
+import GridCard from '../cards/GridCard';
+import {projectInfo as projects} from '../../data/projectInfo';
+import ProjectViewerTitle from './ProjectViewerTitle';
+
 export default function ProjectsGrid() {
-  return <div></div>;
+  return (
+    <div>
+      {/* 팀 프로젝트 */}
+      <div className='flex justify-center'>
+        <div className='flex w-[87%]'>
+          {projects
+            .filter((project) => project.type === 'team')
+            .map((project, index) => (
+              <GridCard key={project.id} project={project} index={index + 1} />
+            ))}
+        </div>
+      </div>
+
+      {/* 개인 프로젝트 섹션 제목 */}
+      <ProjectViewerTitle stackedView={false} titleText={'ROOKIE PROJECT'} />
+
+      {/* 개인 프로젝트 */}
+      <div className='flex justify-center'>
+        <div className='grid w-[87%] grid-cols-4'>
+          {projects
+            .filter((project) => project.type === 'rookie')
+            .map((project, index) => (
+              <GridCard key={project.id} project={project} index={index + 1} />
+            ))}
+        </div>
+      </div>
+    </div>
+  );
 }
