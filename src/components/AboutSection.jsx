2a7fd57e49ea58ce65abb46cf35d7215fc30f7ea
tjsl0607 <munyeseon@sookmyung.ac.kr>
2025-12-23 11:27:40 +0900

    feat(about): About Us 섹션 기본 컴포넌트(파란색 배경, 주황색 컨테이너) 생성 및 레이아웃 구현
    Refs #2

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,15 @@
+// src/components/AboutSection.jsx
+
+const AboutSection = () => {
+  return (
+    <section className='w-full bg-[#2A27E8] py-20'>
+      <div className='mx-auto min-h-[400px] max-w-6xl bg-[#F75820]'>
+        <h2 className='p-10 text-center text-3xl font-bold text-white'>
+          WHAT IS APPS?
+        </h2>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
