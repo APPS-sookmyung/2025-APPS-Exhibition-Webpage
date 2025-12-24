6a73c02882e44e7d86d275c1848b55445f569e19
tjsl0607 <munyeseon@sookmyung.ac.kr>
2025-12-25 06:47:46 +0900

    chore: 화면 구성에 MemberIntroSection 추가
    Refs #14

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
 import ProjectsViewer from '../components/sections/ProjectsViewer.jsx';
+import MemberIntroSection from '../components/sections/MemberIntroSection';
 
 export default function MainPage() {
   return (
     <main>
       <ProjectsViewer />
+      <MemberIntroSection />
     </main>
   );
 }
