d3a0f9d0a7fad7b470fefee6ef7d796ff3cb3b41
jimin-25 <2515866@sookmyung.ac.kr>
2025-12-25 17:44:32 +0900

    feat: svg 추가 및 레이아웃 위치 조정

--- src/components/data/activitiesData.js
+++ src/components/data/activitiesData.js
@@ -26,4 +26,17 @@ export const activitiesData = [
     type: 'image',
     image: '/images/devtalk2.jpg',
   },
+  {
+    id: 5,
+    type: 'text',
+    tag: 'SESSION',
+    title: 'DEV TALK',
+    description:
+      '매주 목요일 저녁 6시에 모여 약 1시간 동안 정기 세미나를 진행합니다. 이 시간에는 각 팀별 스터디와 프로젝트의 진행 상황을 발표하고, 10-15부 정도 DevTalk을 진행합니다.',
+  },
+  {
+    id: 6,
+    type: 'image',
+    image: '/images/devtalk3.jpg',
+  },
 ];
