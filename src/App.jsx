deee99cb0a538248b20c97130e61732f90052759
tjsl0607 <munyeseon@sookmyung.ac.kr>
2025-12-25 13:52:16 +0900

    chore: 책임에 맞는 컴포넌트 위치 재조정
    Refs #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 // src/App.jsx
 import {useRoutes} from 'react-router-dom';
 import MainPage from './pages/MainPage';
-import AboutSection from './components/sections/AboutSection';
 
 export default function App() {
-  return useRoutes([
-    {path: '/', element: <MainPage />},
-    {path: '/about', element: <AboutSection />},
-  ]);
+  return useRoutes([{path: '/', element: <MainPage />}]);
 }
