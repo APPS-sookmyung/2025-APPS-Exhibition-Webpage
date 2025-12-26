a6bd6783cbd8c07ffc2a17a92128477c8e3fba52
tjsl0607 <munyeseon@sookmyung.ac.kr>
2025-12-27 02:57:49 +0900

    chore: 패드 반응형 대상 제외
    Refs #29

--- src/components/sections/AboutSection.jsx
+++ src/components/sections/AboutSection.jsx
@@ -37,7 +37,7 @@ const AboutSection = () => {
             WHAT IS APPS?
           </h2>
           <div className='-mx-6 my-5 border-t-2 border-dotted border-white/50 md:-mx-20 md:border-white'></div>
-          <div className='flex flex-col items-center justify-center gap-10 md:flex-row md:gap-10'>
+          <div className='flex flex-col items-center justify-center gap-10 md:gap-10 lg:flex-row'>
             <div className='relative shrink-0'>
               <div className='mt-5 h-[180px] w-full max-w-[400px] overflow-hidden rounded-[90px] md:w-[400px]'>
                 <img
