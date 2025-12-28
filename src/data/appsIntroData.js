43e41389262d9df78bda8f04f649a3ccc73700b0
하지민 <163178666+JiiminHa@users.noreply.github.com>
2025-12-28 22:51:17 +0900

    chore: 부원 정보 업데이트
    Refs #33

--- src/data/appsIntroData.js
+++ src/data/appsIntroData.js
@@ -45,7 +45,7 @@ export const ROW_1_DATA = [
     id: 'r1-3',
     type: 'member',
     name: '김지민',
-    subText: '11기 · Data Science 23',
+    subText: '11기 · Computer Science 24',
     avatarImage: KimJiminImg,
     message:
       'APPS에서 정말 많이 배웠습니다. 작년 APPS 이전의 저는 파이썬의 ㅍ도 몰랐던 시절이였기에, 말로 표현하기 어려울 만큼 성장했다고 할 수 있을 것 같습니다. APPS 부원들과 여러 프로젝트들을 진행하고, 수상까지 이어지는 그 모든 과정이 제게는 정말 소중하고 값진 경험이었습니다. 사랑합니다 APPS❤',
@@ -63,7 +63,7 @@ export const ROW_1_DATA = [
     id: 'r1-5',
     type: 'member',
     name: '하지민',
-    subText: '11기 · Computer Science 23',
+    subText: '11기 · Computer Science 22',
     avatarImage: HaJiminImg,
     message:
       'APPS에서 처음 개발을 시작해 같이 개발하고 얘기할 친구들도 얻어서 정말 기술적인 성장과 소중한 인연을 얻은 것 같아요!',
@@ -81,7 +81,7 @@ export const ROW_1_DATA = [
     id: 'r1-7',
     type: 'member',
     name: '정지은',
-    subText: '12기 · Computer Science 23',
+    subText: '12기 · Computer Science 24',
     avatarImage: JeongJieunImg,
     message:
       '신입부원 프로젝트를 통해 기획부터 개발, 디자인까지 혼자 힘으로 해보며 큰 경험이 되었습니다. 그리고 팀원들과 오픈소스 개발자 대회에 참여하여 처음으로 규모가 큰 개발을 해보며 부족한 점도 많이 깨닫고 더 공부하고 많이 해봐야겠다고 느꼈습니다.',
@@ -94,7 +94,7 @@ export const ROW_2_DATA = [
     id: 'r2-1',
     type: 'member',
     name: '백수민',
-    subText: '11기 · Computer Science 23',
+    subText: '11기 · Computer Science 22',
     avatarImage: BaekSuminImg,
     message:
       'APPS에 들어오기 전의 저는 솔직히 말해 남들 앞에 나서는 것도, 새로운 도전에 뛰어드는 것도 두려워하던 사람이었습니다. 하지만 APPS에서 서로의 성장을 진심으로 응원하는 환경 속에서 지내다 보니 용기를 내어 도전하게 되었습니다. 1년간 APPS 활동을 통해 기술적으로도 한 사람으로서도 한 단계 더 성장할 수 있었습니다. APPS에서의 경험를 바탕으로 내년에도 새로운 환경과 과제들 속에서 계속 배우고, 작은 도전들을 차근차근 이어가며 저만의 속도로 성장해 나가려고 합니다! 고마워요 APPS 😊',
@@ -103,7 +103,7 @@ export const ROW_2_DATA = [
     id: 'r2-2',
     type: 'member',
     name: '김도연',
-    subText: '12기 · Computer Science 24',
+    subText: '12기 · Data Science 23',
     avatarImage: KimDoyeonImg,
     message:
       'APPS 전에 저는 발표하는 것 자체를 부담스러워했는데, APPS에서 대면 발표도 하고 스터디 발표도 자주 하면서 이제는 사람들 앞에서 이야기하는 일이 훨씬 자연스럽고 익숙해졌습니다. 또 새로운 걸 시작할 때 예전처럼 두려워하기보다, 한 번 해보자는 생각으로 더 편하게 도전할 수 있게 된 것 같아요.',
@@ -118,7 +118,7 @@ export const ROW_2_DATA = [
     id: 'r2-4',
     type: 'member',
     name: '김영교',
-    subText: '12기 · Information Security 24',
+    subText: '12기 · Entrepreneurship 23',
     avatarImage: KimYoungkyoImg,
     message:
       '정말 열정적인 분들이 많다는 걸 느꼈어요. 학번이나 나이에 상관없이 모두가 마치 몸이 두 개라도 되는 것처럼 적극적이었고, 그 점에서 부원분들께 많이 배웠습니다. 혼자였다면 절대 해보지 못했을 경험들도 함께해보고, 대회에도 나가면서 조금이나마 성장할 수 있었던 것 같아요.',
@@ -164,7 +164,7 @@ export const ROW_3_DATA = [
     id: 'r3-1',
     type: 'member',
     name: '배송이',
-    subText: '12기 · Visual Design 24',
+    subText: '12기 · Computer Science 24',
     avatarImage: BaeSongyiImg,
     message:
       '개발에 있어 어떤 스택이 필요한지도 거의 모르는 상태에서 APPS에 들어왔는데, 1년동안 개발 지식 뿐만 아니라 방향성까지도 많이 배운 것 같습니다. 기술적인 부족함보다도 뭘 해야 배울 수 있을 지 잘 몰라 막막했는데, 여러 활동을 통해 열심히 사는 부원들을 보고 관련 인사이트를 얻은 것 같아요! 1년동안 정말 많은 것을 배운 앱스! 진로가 막막한 분들에게도, 열정적으로 사는 걸 좋아하는 분들에게도 추천합니다.',
@@ -206,7 +206,7 @@ export const ROW_3_DATA = [
     id: 'r3-6',
     type: 'member',
     name: '이유나',
-    subText: '12기 · Computer Science 23',
+    subText: '12기 · Data Science 25',
     avatarImage: LeeYunaImg,
     message:
       '새내기인 저로서 APPS 활동은 정말 ‘하길 너무 잘했다!’ 싶은 활동이었어요. APPS를 하지 않았다면 제 대학 첫 1년이 얼마나 무의미하게 흘러갔을까 싶어요. 아무것도 모르는 감자였던 저지만, 열심히 공부하고 활동하시는 선배님들 아래에서 저도 자연스럽게 더 배우고 흥미를 붙일 수 있었던 것 같아요. 덕분에 첫 1년을 정말 밀도 있게 보냈다는 게 너무 뿌듯하고, 좋은 사람들을 많이 만날 수 있어서 정말 행복한 활동이었습니다!',
@@ -215,7 +215,7 @@ export const ROW_3_DATA = [
     id: 'r3-7',
     type: 'member',
     name: '한지민',
-    subText: '12기 · Computer Science 23',
+    subText: '12기 · Computer Science 25',
     avatarImage: HanJiminImg,
     message:
       '처음에는 아무것도 몰랐지만 다양한 스터디를 통해 개발의 기초 지식을 쌓고 기술 스택을 함께 공부하며 부족한 부분을 서로 공유할 수 있었습니다. 평소 도전을 두려워하던 저도 열정적이고 뛰어난 사람들을 보며 좋은 자극을 받아 많이 부족하더라도 시도해보려는 도전 자세를 갖게 되었습니다. APPS는 저에게 기술적인 성장뿐만 아니라 개인적인 성장까지 이루게 해준 소중한 경험입니다 !',
